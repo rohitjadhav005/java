5bd4d836b881540420d8a3e6225401102f880c90
rohitjadhav005 <rj8169552@gmail.com>
2026-07-21 00:04:22 +0530

    array element finding program in the arrya

---
+++ Day4/DSA/ArrayIntro.java
@@ -0,0 +1,10 @@
+public class ArrayIntro {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50};
+        arr[0]=12;
+        System.out.println(arr[0]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+    }
+}
