d843cb136bf0ff1d2c64758be0c905a04f21623f
rohitjadhav005 <rj8169552@gmail.com>
2026-08-03 00:01:29 +0530

    added new file

---
+++ Day4/DSA/array6.java
@@ -0,0 +1,11 @@
+public class array6 {
+    public static void main(String[] args) {
+
+        String name = "Rohit";
+
+        // Traverse the string
+        for (int i = 0; i < name.length(); i++) {
+            System.out.println(name.charAt(i));
+        }
+    }
+}
