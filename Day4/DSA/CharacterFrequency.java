e5cda4969e23d9773b6bf20be1ea56d134a6f5e4
rohitjadhav005 <rj8169552@gmail.com>
2026-09-04 17:37:44 +0530

    Added character frequency counter

---
+++ Day4/DSA/CharacterFrequency.java
@@ -0,0 +1,18 @@
+public class CharacterFrequency {
+    public static void main(String[] args) {
+
+        String str = "hello";
+
+        int[] frequency = new int[256];
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            frequency[ch]++;
+        }
+        for (int i = 0; i < frequency.length; i++) {
+            if (frequency[i] > 0) {
+                System.out.println((char) i + " : " + frequency[i]);
+            }
+        }
+    }
+}
